ff06c3053d7c935af50821893145d498e4de0bf6
Ben Girardet <ben@platform5.ch>
2022-02-10 11:15:49 +0100

    Exercise 3: Tweaks to our main view

--- src/components/ShoeIndex/ShoeIndex.js
+++ src/components/ShoeIndex/ShoeIndex.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import styled from 'styled-components/macro';
 
-import { WEIGHTS } from '../../constants';
+import { QUERIES, WEIGHTS } from '../../constants';
 
 import Breadcrumbs from '../Breadcrumbs';
 import Select from '../Select';
@@ -35,7 +35,6 @@ const ShoeIndex = ({ sortId, setSortId }) => {
             Shoes
           </Breadcrumbs.Crumb>
         </Breadcrumbs>
-        <Spacer size={42} />
         <ShoeSidebar />
       </LeftColumn>
     </Wrapper>
@@ -47,10 +46,22 @@ const Wrapper = styled.div`
   flex-direction: row-reverse;
   align-items: baseline;
   gap: 32px;
+
+  @media ${QUERIES.tableAndDown} {
+    flex-direction: column-reverse;
+    gap: 0px;
+  }
 `;
 
 const LeftColumn = styled.div`
   flex-basis: 248px;
+  display: flex;
+  flex-direction: column;
+  gap: 48px;
+
+  @media ${QUERIES.tableAndDown} {
+    flex-basis: auto;
+  }
 `;
 
 const MainColumn = styled.div`
@@ -61,6 +72,12 @@ const Header = styled.header`
   display: flex;
   justify-content: space-between;
   align-items: baseline;
+
+  @media ${QUERIES.tableAndDown} {
+    & > label {
+      display: none;
+    }
+  }
 `;
 
 const Title = styled.h2`
