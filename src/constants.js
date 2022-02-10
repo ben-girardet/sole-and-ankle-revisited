0188739e7f3e9418de4c5a53d317ee666d146d29
Ben Girardet <ben@platform5.ch>
2022-02-10 10:47:07 +0100

    Exercise 1: Set up our breakpoints

--- src/constants.js
+++ src/constants.js
@@ -16,3 +16,14 @@ export const WEIGHTS = {
   medium: 600,
   bold: 800,
 };
+
+const BREAKPOINTS = {
+  phoneMax: 600,
+  tabletMax: 900,
+  laptopMax: 1300,
+}
+export const QUERIES = {
+  'phoneAndDown': `(max-width: ${BREAKPOINTS.phoneMax / 16}rem)`,
+  'tableAndDown': `(max-width: ${BREAKPOINTS.tabletMax / 16}rem)`,
+  'laptopAndDown': `(max-width: ${BREAKPOINTS.laptopMax / 16}rem)`,
+}
